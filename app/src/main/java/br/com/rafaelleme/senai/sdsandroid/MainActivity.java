7bdfa34c3f661670d50d8ac05916ec7a16bb4307
Rafael212001 <rafael_ferreira2013@hotmail.com.br>
2018-11-14 02:55:27 -0200

    inicio

--- app/src/main/java/br/com/rafaelleme/senai/sdsandroid/MainActivity.java
+++ app/src/main/java/br/com/rafaelleme/senai/sdsandroid/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -17,6 +18,8 @@ import br.com.rafaelleme.senai.sdsandroid.entities.ClasseGenerica;
 import br.com.rafaelleme.senai.sdsandroid.entities.ClasseTurma;
 import br.com.rafaelleme.senai.sdsandroid.service.ProfessorService;
 import br.com.rafaelleme.senai.sdsandroid.service.TurmaService;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -28,11 +31,131 @@ public class MainActivity extends AppCompatActivity {
     Spinner spinner2;
     Retrofit retrofit;
 
+
+    @BindView(R.id.s1)
+    TextView s1;
+
+    @BindView(R.id.s2)
+    TextView s2;
+
+
+    @BindView(R.id.s3)
+    TextView s3;
+
+
+    @BindView(R.id.s4)
+    TextView s4;
+
+
+    @BindView(R.id.s5)
+    TextView s5;
+
+
+    @BindView(R.id.t1)
+    TextView t1;
+
+    @BindView(R.id.t2)
+    TextView t2;
+
+
+    @BindView(R.id.t3)
+    TextView t3;
+
+
+    @BindView(R.id.t4)
+    TextView t4;
+
+
+    @BindView(R.id.t5)
+    TextView t5;
+
+
+    @BindView(R.id.q1)
+    TextView q1;
+
+    @BindView(R.id.q2)
+    TextView q2;
+
+
+    @BindView(R.id.q3)
+    TextView q3;
+
+
+    @BindView(R.id.q4)
+    TextView q4;
+
+
+    @BindView(R.id.q5)
+    TextView q5;
+
+
+    @BindView(R.id.qi1)
+    TextView qi1;
+
+    @BindView(R.id.qi2)
+    TextView qi2;
+
+
+    @BindView(R.id.qi3)
+    TextView qi3;
+
+
+    @BindView(R.id.qi4)
+    TextView qi4;
+
+
+    @BindView(R.id.qi5)
+    TextView qi5;
+
+
+    @BindView(R.id.sx1)
+    TextView sx1;
+
+    @BindView(R.id.sx2)
+    TextView sx2;
+
+    @BindView(R.id.sx3)
+    TextView sx3;
+
+    @BindView(R.id.sx4)
+    TextView sx4;
+
+    @BindView(R.id.sx5)
+    TextView sx5;
+
+    @BindView(R.id.sa1)
+    TextView sa1;
+
+    @BindView(R.id.sa2)
+    TextView sa2;
+
+    @BindView(R.id.sa3)
+    TextView sa3;
+
+    @BindView(R.id.sa4)
+    TextView sa4;
+
+    @BindView(R.id.sa5)
+    TextView sa5;
+
+
+
+
+
+
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         retrofit = RetrofitInstance.getInstance();
+
+        ButterKnife.bind(this);
 
         spinner = findViewById(R.id.spinnerTurma);
         spinner2 = findViewById(R.id.spinnerProfessor);
@@ -42,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
                 ClasseGenerica cg = (ClasseGenerica) spinner.getSelectedItem();
-                Log.i("Teste",cg.getId().toString());
+                Log.i("Teste", cg.getId().toString());
             }
 
             @Override
@@ -56,7 +179,7 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
                 ClasseTurma cg = (ClasseTurma) spinner.getSelectedItem();
-                Log.i("Teste",cg.getId().toString());
+                Log.i("Teste", cg.getId().toString());
             }
 
             @Override
@@ -70,12 +193,12 @@ public class MainActivity extends AppCompatActivity {
         buscaProfessor.enqueue(new Callback<List<ClasseGenerica>>() {
             @Override
             public void onResponse(Call<List<ClasseGenerica>> call, Response<List<ClasseGenerica>> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     List<ClasseGenerica> resposta = response.body();
 
                     List<String> lista = new ArrayList<>();
                     lista.add("Selecione");
-                    for (ClasseGenerica c : resposta){
+                    for (ClasseGenerica c : resposta) {
                         lista.add(c.getNome());
                     }
                     ArrayAdapter<ClasseGenerica> adapter = new ArrayAdapter<ClasseGenerica>(MainActivity.this, R.layout.support_simple_spinner_dropdown_item, resposta.toArray(new ClasseGenerica[resposta.size()]));
@@ -98,12 +221,12 @@ public class MainActivity extends AppCompatActivity {
         buscaTurma.enqueue(new Callback<List<ClasseTurma>>() {
             @Override
             public void onResponse(Call<List<ClasseTurma>> call, Response<List<ClasseTurma>> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     List<ClasseTurma> resposta = response.body();
 
                     List<String> lista = new ArrayList<>();
                     lista.add("Selecione");
-                    for (ClasseTurma c : resposta){
+                    for (ClasseTurma c : resposta) {
                         lista.add(c.getNome());
                     }
                     ArrayAdapter<ClasseTurma> adapter = new ArrayAdapter<ClasseTurma>(MainActivity.this, R.layout.support_simple_spinner_dropdown_item, resposta.toArray(new ClasseTurma[resposta.size()]));
